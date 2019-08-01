9b39573af5e0fec2d02dd7009d052a24d8ef378c
Mellisa Madrid <mellisamadrid@hotmail.com>
2019-08-01 10:46:46 +0100

    first commit

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    chirps: [
+        {
+            userHandle: 'user',
+            body: 'this is the chirp body',
+            createdAt: "2019-07-31T12:30:13.764Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports  = {
+    apiKey: "AIzaSyAPm0Tg2CvSqvNzOK30mKss7EoiZSpwjn8",
+    authDomain: "chirpy-1dcff.firebaseapp.com",
+    databaseURL: "https://chirpy-1dcff.firebaseio.com",
+    projectId: "chirpy-1dcff",
+    storageBucket: "chirpy-1dcff.appspot.com",
+    messagingSenderId: "619481866839",
+    appId: "1:619481866839:web:4d05d964b2981aca"
+  };
