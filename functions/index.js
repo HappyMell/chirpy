f90987cbfa680b25ccecdd0ed342361ea078244a
Mellisa Madrid <mellisamadrid@hotmail.com>
2019-08-05 10:51:05 +0100

    firebase DB made

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,165 @@
 const functions = require('firebase-functions');
 const app = require('express')()
 const FBAuth = require('./util/fbAuth');
-const {getAllChirps, postOneChirp} = require('./handlers/chirps');
-const {signUp, login} = require('./handlers/users');
-
-
-
+const { db } = require('./util/admin');
+const cors = require('cors');
+app.use(cors());
+const {
+    getAllChirps,
+    postOneChirp,
+    getChirp,
+    commentOnChirp,
+    likeChirp,
+    unlikeChirp,
+    deleteChirp
+} = require('./handlers/chirps');
+const {
+    signUp,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser,
+    getUserDetails,
+    markNotificationsRead
+} = require('./handlers/users');
 
 //Chirp Routes
 app.get('/chirps', getAllChirps);
 app.post('/chirp', FBAuth, postOneChirp);
+app.get('/chirp/:chirpId', getChirp);
+app.post('/chirp/:chirpId/comment', FBAuth, commentOnChirp)
+app.get('/chirp/:chirpId/like', FBAuth, likeChirp);
+app.get('/chirp/:chirpId/unlike', FBAuth, unlikeChirp)
+app.delete('/chirp/:chirpId', FBAuth, deleteChirp);
 
-//Login and Signup route
+
+
+//Login and Signup route and Users
 app.post('/signup', signUp)
 app.post('/login', login)
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 
+exports.api = functions.region('europe-west2').https.onRequest(app);
 
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.createNotificationOnLike = functions
+    .region('europe-west2')
+    .firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        return db
+            .doc(`/chirps/${snapshot.data().chirpId}`)
+            .get()
+            .then((doc) => {
+                if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        chirpId: doc.id
+                    });
+                }
+            })
+            .catch((err) => console.error(err));
+    });
+
+exports.deleteNotificationOnUnLike = functions
+    .region('europe-west2')
+    .firestore.document('likes/{id}')
+    .onDelete((snapshot) => {
+        return db
+            .doc(`/notifications/${snapshot.id}`)
+            .delete()
+            .catch((err) => {
+                console.error(err);
+                return;
+            });
+    });
+
+exports.createNotificationOnComment = functions
+    .region('europe-west2')
+    .firestore.document('comments/{id}')
+    .onCreate((snapshot) => {
+        return db
+            .doc(`/chirps/${snapshot.data().chirpId}`)
+            .get()
+            .then((doc) => {
+                if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        chirpId: doc.id
+                    });
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                return;
+            });
+    });
+
+exports.onUserImageChange = functions.region('europe-west2').firestore.document('/users/{userId}')
+    .onUpdate((change) => {
+        console.log(change.before.data());
+        console.log(change.after.data());
+        if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+            console.log('Image has changed');
+            const batch = db.batch();
+            return db.collection('chirps')
+                .where('userHandle', '==', change.before.data().handle)
+                .get()
+                .then((data) => {
+                    data.forEach(doc => {
+                        const chirp = db.doc(`/chirps/${doc.id}`);
+                        batch.update(chirp, { userImage: change.after.data().imageUrl });
+                    })
+                    return batch.commit();
+                })
+        } else {
+            return true;
+        }
+
+    });
+
+
+exports.onChirpDelete = functions.region('europe-west2').firestore.document('/chirps/{chirpId}')
+    .onDelete((snapshot, context) => {
+        const chirpId = context.params.chirpId;
+        const batch = db.batch();
+        return db.collection('comments')
+            .where('chripId', '==', chirpId)
+            .get()
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/comments/${doc.id}`));
+                })
+                return db.collection('likes')
+                    .where('chirpId', '==', chirpId)
+                    .get()
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/likes/${doc.id}`));
+                })
+                return db.collection('notifications')
+                    .where('chirpId', '==', chirpId)
+                    .get()
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/notifications/${doc.id}`));
+                })
+                return batch.commit();
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    })    
--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports  = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    apiKey: "AIzaSyAPm0Tg2CvSqvNzOK30mKss7EoiZSpwjn8",
+    authDomain: "chirpy-1dcff.firebaseapp.com",
+    databaseURL: "https://chirpy-1dcff.firebaseio.com",
+    projectId: "chirpy-1dcff",
+    storageBucket: "chirpy-1dcff.appspot.com",
+    messagingSenderId: "619481866839",
   
   };
