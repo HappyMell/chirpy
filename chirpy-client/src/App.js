9b6d2356cae0388aba9be365618cba13e8efa966
Mellisa Madrid <mellisamadrid@hotmail.com>
2019-08-06 08:16:42 +0100

    React client start

---
+++ chirpy-client/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
+//Components
+import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
+
+//Pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+const theme = createMuiTheme(themeFile);
+let authenticated;
+const token = localStorage.FBIdToken;
+
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login'
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <AuthRoute exact path='/login' component={Login} authenticated={authenticated} />
+                <AuthRoute exact Path='/signup' component={Signup} authenticated={authenticated} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ chirpy-client/src/pages/home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Chirp from '../components/Chirp';
+
+class Home extends Component {
+    state = {
+        chirps: null
+    }
+
+    componentDidMount() {
+        axios
+            .get('/chirps')
+            .then(res => {
+                this.setState({
+                    chirps: res.data
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        let recentChirpsMarkup = this.state.chirps ? (
+            this.state.chirps.map(chirp => <Chirp key={chirp.chirpId} chirp={chirp} />)
+        ) : <p>Loading...</p>
+        return (
+            <Grid container spacing={2}>
+                <Grid item sm={8} xs={12}>
+                    {recentChirpsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
+
+        )
+    }
+}
+
+export default Home
